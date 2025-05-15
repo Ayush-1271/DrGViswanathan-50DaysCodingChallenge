4cb622edc914b980211995a93abfb25c2d24252c
Ayush Ranjan <87318600+Ayush-1271@users.noreply.github.com>
2025-05-15 20:15:32 +0530

    Create sum-of-square-numbers.c

---
+++ Day 16/sum-of-square-numbers.c
@@ -0,0 +1,16 @@
+bool judgeSquareSum(int c) {
+    long long a = 0;
+    long long b = (long long)sqrt(c);  // Use long long to avoid overflow
+
+    while (a <= b) {
+        long long sum = a * a + b * b;
+        if (sum == c) {
+            return true;
+        } else if (sum < c) {
+            a++;
+        } else {
+            b--;
+        }
+    }
+    return false;
+}
