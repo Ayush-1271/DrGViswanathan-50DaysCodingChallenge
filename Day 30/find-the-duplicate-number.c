db94b157aa3031993b7f93af01fb892b28013375
Ayush Ranjan <87318600+Ayush-1271@users.noreply.github.com>
2025-05-29 20:47:22 +0530

    Create find-the-duplicate-number.c

---
+++ Day 30/find-the-duplicate-number.c
@@ -0,0 +1,19 @@
+int findDuplicate(int* nums, int numsSize) {
+    int slow = nums[0];
+    int fast = nums[0];
+
+    // Phase 1: Find intersection point
+    do {
+        slow = nums[slow];
+        fast = nums[nums[fast]];
+    } while (slow != fast);
+
+    // Phase 2: Find entrance to cycle (duplicate)
+    slow = nums[0];
+    while (slow != fast) {
+        slow = nums[slow];
+        fast = nums[fast];
+    }
+
+    return slow;
+}
