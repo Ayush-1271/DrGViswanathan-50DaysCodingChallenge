473caec4d628239f26b95a30fa7bd5acae6683bc
Ayush Ranjan <87318600+Ayush-1271@users.noreply.github.com>
2025-06-03 22:57:43 +0530

    Create lowest-common-ancestor-of-a-binary-tree.c

---
+++ Day 35/lowest-common-ancestor-of-a-binary-tree.c
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+struct TreeNode* lowestCommonAncestor(struct TreeNode* root, struct TreeNode* p, struct TreeNode* q) {
+    if (root == NULL || root == p || root == q) {
+        return root;
+    }
+
+    struct TreeNode* left = lowestCommonAncestor(root->left, p, q);
+    struct TreeNode* right = lowestCommonAncestor(root->right, p, q);
+
+    if (left && right) {
+        return root;  // p and q are found in different subtrees
+    }
+
+    return left ? left : right;  // one of them is NULL, return the non-null
+}
