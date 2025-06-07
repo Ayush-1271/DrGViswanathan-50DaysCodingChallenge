1d490bc3a24edd119a6904fdf20328792a3d2b73
Ayush Ranjan <87318600+Ayush-1271@users.noreply.github.com>
2025-06-07 19:33:49 +0530

    Create insert-into-a-BST.c

---
+++ Day 39/insert-into-a-BST.c
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+struct TreeNode* insertIntoBST(struct TreeNode* root, int val) {
+    // If the tree is empty, insert here
+    if (root == NULL) {
+        struct TreeNode* newNode = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+        newNode->val = val;
+        newNode->left = NULL;
+        newNode->right = NULL;
+        return newNode;
+    }
+
+    // Traverse the BST based on value
+    if (val < root->val) {
+        root->left = insertIntoBST(root->left, val);
+    } else {
+        root->right = insertIntoBST(root->right, val);
+    }
+
+    return root;
+}
